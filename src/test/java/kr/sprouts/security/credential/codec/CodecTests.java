6f8051c30d74448d8f1ccf25aa05591ff2b3673a
Jinwon Seo <jwseo@live.co.kr>
2023-08-03 11:23:21 +0900

    Codec test 추가.

---
+++ src/test/java/kr/sprouts/security/credential/codec/CodecTests.java
@@ -0,0 +1,29 @@
+package kr.sprouts.security.credential.codec;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CodecTests {
+    Logger log = Logger.getLogger(this.getClass().getName());
+
+    @Test
+    void encodeAndDecode() {
+        byte[] testBytes = "encode and decode test text.".getBytes(StandardCharsets.UTF_8);
+
+        for (CodecType codecType : CodecType.values()) {
+            Codec codec = codecType.getCodecSupplier().get();
+
+            String encodedText = codec.encodeToString(testBytes);
+            log.info("EncodedText: " + encodedText);
+            byte[] decodedBytes = codec.decode(encodedText);
+            String decodedString = new String(decodedBytes);
+            log.info("DecodedString : " + decodedString);
+
+            assertEquals("encode and decode test text.", decodedString);
+        }
+    }
+}
