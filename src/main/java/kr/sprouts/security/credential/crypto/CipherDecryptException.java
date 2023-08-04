b34263b84fe3bde702f6f5cb35e4ad1caa3bdd7b
Jinwon Seo <jwseo@live.co.kr>
2023-08-04 10:52:41 +0900

    Cipher 추가.

---
+++ src/main/java/kr/sprouts/security/credential/crypto/CipherDecryptException.java
@@ -0,0 +1,8 @@
+package kr.sprouts.security.credential.crypto;
+
+class CipherDecryptException extends RuntimeException {
+    CipherDecryptException() { }
+    CipherDecryptException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/test/java/kr/sprouts/security/credential/crypto/CipherTests.java
@@ -0,0 +1,80 @@
+package kr.sprouts.security.credential.crypto;
+
+import kr.sprouts.security.credential.codec.Codec;
+import kr.sprouts.security.credential.codec.CodecType;
+import org.junit.jupiter.api.Test;
+
+import javax.crypto.SecretKey;
+import java.security.KeyPair;
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CipherTests {
+    Logger log = Logger.getLogger(this.getClass().getName());
+    Codec codec = CodecType.BASE64_URL.getCodecSupplier().get();
+    String plainText = "Plain text.";
+
+    @Test
+    void encryptAndDecrypt() {
+
+        for (CipherAlgorithm cipherAlgorithm : CipherAlgorithm.values()) {
+            log.info("name : " + cipherAlgorithm.name());
+
+            Cipher<?> cipher = cipherAlgorithm.getCipherSupplier().get();
+
+            Object secret = cipher.generateSecret();
+
+            if (secret instanceof SecretKey) {
+                SecretKey secretKey = (SecretKey) secret;
+
+                String encodedSecretKey = codec.encodeToString(secretKey.getEncoded());
+                log.info("EncodedSecretKey : " + encodedSecretKey);
+
+                byte[] encryptedText = cipher.encrypt(plainText, secretKey.getEncoded());
+                String encodeAndEncryptedText = codec.encodeToString(encryptedText);
+                log.info("EncodeAndEncryptedText : " + encodeAndEncryptedText);
+
+                String decryptedText = cipher.decryptToString(encryptedText, secretKey.getEncoded());
+                log.info(decryptedText);
+
+                assertEquals(plainText, decryptedText);
+            } else if (secret instanceof byte[]) {
+                byte[] password = (byte[]) secret;
+
+                String encodedPassword = codec.encodeToString(password);
+                log.info("EncodedPassword : " + encodedPassword);
+
+                byte[] encryptedText = cipher.encrypt(plainText, password);
+                String encodeAndEncryptedText = codec.encodeToString(encryptedText);
+                log.info("EncodeAndEncryptedText" + encodeAndEncryptedText);
+
+                String decryptedText = cipher.decryptToString(encryptedText, password);
+                log.info(decryptedText);
+
+                assertEquals(plainText, decryptedText);
+            } else if (secret instanceof KeyPair) {
+                KeyPair keyPair = (KeyPair) secret;
+
+                byte[] publicKey = keyPair.getPublic().getEncoded();
+                byte[] privateKey = keyPair.getPrivate().getEncoded();
+
+                String encodedPublicKey = codec.encodeToString(publicKey);
+                String encodedPrivateKey = codec.encodeToString(privateKey);
+
+                log.info("EncodedPublicKey : " + encodedPublicKey);
+                log.info("EncodedPrivateKey : " + encodedPrivateKey);
+
+                byte[] encryptedText = cipher.encrypt(plainText, privateKey);
+                String encodeAndEncryptedText = codec.encodeToString(encryptedText);
+
+                log.info("EncodeAndEncryptedText: " + encodeAndEncryptedText);
+
+                String decryptedText = cipher.decryptToString(encryptedText, publicKey);
+                log.info(decryptedText);
+
+                assertEquals(plainText, decryptedText);
+            }
+        }
+    }
+}
