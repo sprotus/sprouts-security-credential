0d989f6bb00d63b6768b02cfd5afab9b973567ab
Jinwon Seo <jwseo@live.co.kr>
2023-08-08 16:10:46 +0900

    Credential 구조 재 구성을 위한 제거.

--- src/main/java/kr/sprouts/security/credential/CredentialConsumer.java
+++
@@ -1,5 +0,0 @@
-package kr.sprouts.security.credential;
-
-public interface CredentialConsumer<T extends Credential<?>> {
-    Principal consume(T credential, byte[] secret);
-}
--- src/main/java/kr/sprouts/security/credential/CredentialProvider.java
+++
@@ -1,5 +0,0 @@
-package kr.sprouts.security.credential;
-
-public interface CredentialProvider<S extends CredentialParameter<? extends Principal>, T extends Credential<?>> {
-    T provide(S credentialParam, byte[] secret);
-}
