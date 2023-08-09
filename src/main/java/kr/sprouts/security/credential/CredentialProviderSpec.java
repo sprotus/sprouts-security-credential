57e84f6499e05f7aca98d2a5ab05944161a8c9ed
Jinwon Seo <jwseo@live.co.kr>
2023-08-10 08:53:04 +0900

    Resolve duplicate. Closed #13

--- src/main/java/kr/sprouts/security/credential/CredentialProviderSpec.java
+++ src/main/java/kr/sprouts/security/credential/CredentialProviderSpec.java
@@ -2,73 +2,14 @@ package kr.sprouts.security.credential;
 
 import java.util.List;
 
-public class CredentialProviderSpec {
-    private String id;
-    private String name;
-    private String type;
-    private String algorithm;
-    private String codec;
-    private String encodedSecret;
+public class CredentialProviderSpec extends CredentialSpec {
     private List<TargetConsumer> targetConsumers;
 
-    public CredentialProviderSpec() { }
+    private CredentialProviderSpec() { }
 
     public CredentialProviderSpec(String id, String name, String type, String algorithm, String codec, String encodedSecret, List<TargetConsumer> targetConsumers) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.algorithm = algorithm;
-        this.codec = codec;
-        this.encodedSecret = encodedSecret;
+        super(id, name, type, algorithm, codec, encodedSecret);
         this.targetConsumers = targetConsumers;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getAlgorithm() {
-        return algorithm;
-    }
-
-    public void setAlgorithm(String algorithm) {
-        this.algorithm = algorithm;
-    }
-
-    public String getCodec() {
-        return codec;
-    }
-
-    public void setCodec(String codec) {
-        this.codec = codec;
-    }
-
-    public String getEncodedSecret() {
-        return encodedSecret;
-    }
-
-    public void setEncodedSecret(String encodedSecret) {
-        this.encodedSecret = encodedSecret;
     }
 
     public List<TargetConsumer> getTargetConsumers() {
@@ -83,7 +24,7 @@ public class CredentialProviderSpec {
         private String id;
         private String name;
 
-        public TargetConsumer() { }
+        private TargetConsumer() { }
 
         public TargetConsumer(String id, String name) {
             this.id = id;
