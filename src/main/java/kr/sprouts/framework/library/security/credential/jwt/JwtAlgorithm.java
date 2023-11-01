38d5a04d169dbb4c91bff098c551431f67b9fb6c
jinwonseo <jwseo@live.co.kr>
2023-11-01 09:17:24 +0900

    Resolve java:S1192 Closed #51

--- src/main/java/kr/sprouts/framework/library/security/credential/jwt/JwtAlgorithm.java
+++ src/main/java/kr/sprouts/framework/library/security/credential/jwt/JwtAlgorithm.java
@@ -10,16 +10,19 @@ public enum JwtAlgorithm {
     HS256("HS256", () -> new JwtWithSecretKey(Jwts.SIG.HS256)),
     HS384("HS384", () -> new JwtWithSecretKey(Jwts.SIG.HS384)),
     HS512("HS512", () -> new JwtWithSecretKey(Jwts.SIG.HS512)),
-    RS256("RS256", () -> new JwtWithKeyPair(Jwts.SIG.RS256, "RSA")),
-    RS384("RS384", () -> new JwtWithKeyPair(Jwts.SIG.RS384, "RSA")),
-    RS512("RS512", () -> new JwtWithKeyPair(Jwts.SIG.RS512, "RSA")),
-    ES256("ES256", () -> new JwtWithKeyPair(Jwts.SIG.ES256, "ECDSA")),
-    ES384("ES384", () -> new JwtWithKeyPair(Jwts.SIG.ES384, "ECDSA")),
-    ES512("ES512", () -> new JwtWithKeyPair(Jwts.SIG.ES512, "ECDSA")),
-    PS256("PS256", () -> new JwtWithKeyPair(Jwts.SIG.PS256, "RSA")),
-    PS384("PS384", () -> new JwtWithKeyPair(Jwts.SIG.PS384, "RSA")),
-    PS512("PS512", () -> new JwtWithKeyPair(Jwts.SIG.PS512, "RSA")),
+    RS256("RS256", () -> new JwtWithKeyPair(Jwts.SIG.RS256, JwtAlgorithm.RSA_FAMILY_NAME)),
+    RS384("RS384", () -> new JwtWithKeyPair(Jwts.SIG.RS384, JwtAlgorithm.RSA_FAMILY_NAME)),
+    RS512("RS512", () -> new JwtWithKeyPair(Jwts.SIG.RS512, JwtAlgorithm.RSA_FAMILY_NAME)),
+    ES256("ES256", () -> new JwtWithKeyPair(Jwts.SIG.ES256, JwtAlgorithm.ECDSA_FAMILY_NAME)),
+    ES384("ES384", () -> new JwtWithKeyPair(Jwts.SIG.ES384, JwtAlgorithm.ECDSA_FAMILY_NAME)),
+    ES512("ES512", () -> new JwtWithKeyPair(Jwts.SIG.ES512, JwtAlgorithm.ECDSA_FAMILY_NAME)),
+    PS256("PS256", () -> new JwtWithKeyPair(Jwts.SIG.PS256, JwtAlgorithm.RSA_FAMILY_NAME)),
+    PS384("PS384", () -> new JwtWithKeyPair(Jwts.SIG.PS384, JwtAlgorithm.RSA_FAMILY_NAME)),
+    PS512("PS512", () -> new JwtWithKeyPair(Jwts.SIG.PS512, JwtAlgorithm.RSA_FAMILY_NAME)),
     ;
+
+    private static final String RSA_FAMILY_NAME = "RSA";
+    private static final String ECDSA_FAMILY_NAME = "ECDSA";
 
     JwtAlgorithm(String name, Supplier<Jwt<?>> jwtSupplier) {
         this.name = name;
